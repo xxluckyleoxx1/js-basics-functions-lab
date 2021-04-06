179fac73417c4da38b400ad73932bc508f66a522
Leonides <xxluckyleoxx1@gmail.com>
2021-04-06 00:40:06 -0400

    commit tree f48d570c4abcf7f8e74890d137a51daa6aeccec2

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNum){
+    if(blockNum > 42){
+        return blockNum - 42;
+    }
+    if(blockNum < 42){
+        return 42 - blockNum;
+    }
+    else{
+        return 0;
+    }
+}
+function distanceFromHqInFeet(blockNum){
+    return distanceFromHqInBlocks(blockNum) * 264;
+}
+function distanceTravelledInFeet(block1, block2){
+    if(block1 > block2){
+        return (block1 - block2) * 264;
+    }
+    if(block1 < block2){
+        return (block2 - block1) * 264;
+    }
+    else{
+        return 0;
+    }
+
+}
+function calculatesFarePrice(start, destination){
+    if(distanceTravelledInFeet(start, destination) <= 400){
+        return 0;
+    }
+    if(distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <=2000){
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    }
+    if(distanceTravelledInFeet(start, destination) > 2000){
+        if(distanceTravelledInFeet(start, destination) > 2500){
+            return 'cannot travel that far';
+        }
+        else{
+            return 25;
+        }
+    }
+
+}
